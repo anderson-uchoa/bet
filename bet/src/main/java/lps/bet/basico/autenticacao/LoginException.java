4729fe6c73e9f035229fa5943576add9367535dd
otaviomacedo <otaviomacedo@39113a39-0f3c-0410-881d-df7b18055427>
2007-10-10 01:29:16 +0000

    Inclusão de classe para tratar exeção de login.

---
+++ bet/src/main/java/lps/bet/basico/autenticacao/LoginException.java
@@ -0,0 +1,25 @@
+package lps.bet.basico.autenticacao;
+
+public class LoginException extends Exception{
+
+	public LoginException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public LoginException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
