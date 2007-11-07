dc86e2c7581f236853d1b6b581636ad6210f5768
pauladonegan <pauladonegan@39113a39-0f3c-0410-881d-df7b18055427>
2007-11-07 23:01:56 +0000

    Insercao de componentes opcionais das variabilidades referentes a Tempo, NumViagens e inicio de implementacao do LinhaIntegradaMgr e do aspecto para tratar ValidarTempo

---
+++ bet/src/main/java/lps/bet/variabilidades/tiposDados/LinhaIntegrada.java
@@ -0,0 +1,23 @@
+package lps.bet.variabilidades.tiposDados;
+
+public class LinhaIntegrada {
+
+	private int linhaIntegradaID;
+	private int linhaOriginalID;
+	
+	public int getLinhaIntegradaID() {
+		return linhaIntegradaID;
+	}
+
+	public void setLinhaIntegradaID(int linhaIntegradaID) {
+		this.linhaIntegradaID = linhaIntegradaID;
+	}
+
+	public int getLinhaOriginalID() {
+		return linhaOriginalID;
+	}
+
+	public void setLinhaOriginalID(int linhaOriginalID) {
+		this.linhaOriginalID = linhaOriginalID;
+	}	
+}
---
+++ bet/src/main/java/lps/bet/variabilidades/tiposDados/SistViarioUrbanoNumViagens.java
@@ -0,0 +1,20 @@
+package lps.bet.variabilidades.tiposDados;
+
+public class SistViarioUrbanoNumViagens {
+
+	private int numViagensID;
+	private int numViagens;
+	
+	public int getNumViagensID() {
+		return numViagensID;
+	}
+	public void setNumViagensID(int numViagensID) {
+		this.numViagensID = numViagensID;
+	}
+	public int getNumViagens() {
+		return numViagens;
+	}
+	public void setNumViagens(int numViagens) {
+		this.numViagens = numViagens;
+	}	
+}
