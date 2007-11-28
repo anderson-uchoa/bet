97f0dcf1338d2bf69407cecd1953256eb9d1bf14
alexdba <alexdba@gmail.com>
2007-11-28 13:44:47 +0000

    Metodo para buscar os tipos permitidos, levando em consideracao os tipos incompativeis da variabilidade Combinacao de Cartoes

--- bet/src/main/java/lps/bet/basico/cartaoMgr/TipoPassageiroDAO.java
+++ bet/src/main/java/lps/bet/basico/cartaoMgr/TipoPassageiroDAO.java
@@ -1,24 +1,27 @@
 package lps.bet.basico.cartaoMgr;
 
+import java.util.Iterator;
 import java.util.List;
 
 import lps.bet.basico.tiposDados.TipoPassageiro;
 
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
-public class TipoPassageiroDAO extends HibernateDaoSupport{
+public class TipoPassageiroDAO extends HibernateDaoSupport {
 
 	String hqlBuscarTipoPassagPorCartao;
 	String hqlBuscarTipoPorNome;
-	
+	private Boolean combinarCartoes = false;
+
 	public String getHqlBuscarTipoPassagPorCartao() {
 		return hqlBuscarTipoPassagPorCartao;
 	}
 
-	public void setHqlBuscarTipoPassagPorCartao(String hqlBuscarTipoPassagPorCartao) {
+	public void setHqlBuscarTipoPassagPorCartao(
+			String hqlBuscarTipoPassagPorCartao) {
 		this.hqlBuscarTipoPassagPorCartao = hqlBuscarTipoPassagPorCartao;
 	}
-		
+
 	public String getHqlBuscarTipoPorNome() {
 		return hqlBuscarTipoPorNome;
 	}
@@ -29,62 +32,79 @@ public class TipoPassageiroDAO extends HibernateDaoSupport{
 
 	public void salvarTipoPassageiro(TipoPassageiro tipo) {
 		getHibernateTemplate().saveOrUpdate(tipo);
-    }
-	
-	public void criarTipoPassageiro(TipoPassageiro tipo){
-		salvarTipoPassageiro(tipo);
 	}
-	
-	public void alterarTipoPassageiro(TipoPassageiro tipo){
+
+	public void criarTipoPassageiro(TipoPassageiro tipo) {
 		salvarTipoPassageiro(tipo);
 	}
 
-	public TipoPassageiro buscarTipoPassageiro(int tipoID){
-		return (TipoPassageiro) getHibernateTemplate().get(TipoPassageiro.class, new Integer(tipoID));
+	public void alterarTipoPassageiro(TipoPassageiro tipo) {
+		salvarTipoPassageiro(tipo);
 	}
-	
-	public List buscarTiposPassageiros(){
+
+	public TipoPassageiro buscarTipoPassageiro(int tipoID) {
+		return (TipoPassageiro) getHibernateTemplate().get(
+				TipoPassageiro.class, new Integer(tipoID));
+	}
+
+	public List buscarTiposPassageiros() {
 		return getHibernateTemplate().loadAll(TipoPassageiro.class);
 	}
-	
-    public TipoPassageiro buscarTipoPassageiro(String nomeTipo){
-		List tipos = getHibernateTemplate().findByNamedParam(hqlBuscarTipoPorNome, "nomeTipo", nomeTipo);
-    	return (TipoPassageiro) tipos.get(0);    	
-    }
-	
-	public void removerTipoPassageiro(TipoPassageiro tipo){
+
+	public TipoPassageiro buscarTipoPassageiro(String nomeTipo) {
+		List tipos = getHibernateTemplate().findByNamedParam(
+				hqlBuscarTipoPorNome, "nomeTipo", nomeTipo);
+		return (TipoPassageiro) tipos.get(0);
+	}
+
+	public void removerTipoPassageiro(TipoPassageiro tipo) {
 		getHibernateTemplate().delete(tipo);
 	}
-	
-	public TipoPassageiro buscarTipoPassagPorCartao(int cartaoID){
-		List tipos = getHibernateTemplate().find(hqlBuscarTipoPassagPorCartao, new Integer(cartaoID));
-		return (TipoPassageiro) tipos.get(0);		
+
+	public TipoPassageiro buscarTipoPassagPorCartao(int cartaoID) {
+		List tipos = getHibernateTemplate().find(hqlBuscarTipoPassagPorCartao,
+				new Integer(cartaoID));
+		return (TipoPassageiro) tipos.get(0);
 	}
-	
-	public List buscarTodosTipos(){
+
+	public List buscarTodosTipos() {
 		return getHibernateTemplate().loadAll(TipoPassageiro.class);
 	}
-	
-	public List buscarTiposPermitidos(List tiposJaAdquiridos){
-//		List todosTipos = buscarTodosTipos();
-//		List tiposPermitidos = new ArrayList();
 
-		List tiposPermitidos = buscarTodosTipos(); 
+	public List buscarTiposPermitidos(List tiposJaAdquiridos) {
+		// List todosTipos = buscarTodosTipos();
+		// List tiposPermitidos = new ArrayList();
+
+		List tiposPermitidos = buscarTodosTipos();
 		tiposPermitidos.removeAll(tiposJaAdquiridos);
-		
-		
-//		for (Iterator it = todosTipos.iterator(); it.hasNext();) {
-//			TipoPassageiro tipo = (TipoPassageiro) it.next();
-//			if (!tiposJaAdquiridos.contains(tipo)){
-//				tiposPermitidos.add(tipo);
-//			}
-//		}
+
+		if (getCombinarCartoes())
+		for (Iterator it = tiposJaAdquiridos.iterator(); it.hasNext();) {
+			TipoPassageiro tipo = (TipoPassageiro) it.next();
+			tiposPermitidos.removeAll(tipo.getTiposIncompativeis());
+		}
+
+		// for (Iterator it = todosTipos.iterator(); it.hasNext();) {
+		// TipoPassageiro tipo = (TipoPassageiro) it.next();
+		// if (!tiposJaAdquiridos.contains(tipo)){
+		// tiposPermitidos.add(tipo);
+		// }
+		// }
+
 		return tiposPermitidos;
 	}
-	
-	public void removerTipoPassageiro(int tipoID){
+
+	public void removerTipoPassageiro(int tipoID) {
 		TipoPassageiro tipo = buscarTipoPassageiro(tipoID);
 		removerTipoPassageiro(tipo);
 	}
-	
+
+	public void setCombinarCartoes(Boolean combinarCartoes) {
+		this.combinarCartoes = combinarCartoes;
+	}
+
+	public Boolean getCombinarCartoes() {
+		return combinarCartoes;
+	}
+
 }
