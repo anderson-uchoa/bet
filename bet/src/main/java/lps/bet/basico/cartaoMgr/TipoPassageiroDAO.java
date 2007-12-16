687351aa7549e39948cd516232bd45579716e5f6
otaviomacedo <otaviomacedo@39113a39-0f3c-0410-881d-df7b18055427>
2007-12-16 01:45:45 +0000

    Substituição de HQLs por critérios do hibernate para buscar tipos de passageiros.

--- bet/src/main/java/lps/bet/basico/cartaoMgr/TipoPassageiroDAO.java
+++ bet/src/main/java/lps/bet/basico/cartaoMgr/TipoPassageiroDAO.java
@@ -1,110 +1,65 @@
 package lps.bet.basico.cartaoMgr;
 
-import java.util.Iterator;
-import java.util.List;
-
 import lps.bet.basico.tiposDados.TipoPassageiro;
-
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import java.util.List;
 
 public class TipoPassageiroDAO extends HibernateDaoSupport {
 
-	String hqlBuscarTipoPassagPorCartao;
-	String hqlBuscarTipoPorNome;
-	private Boolean combinarCartoes = false;
+    public void salvarTipoPassageiro(TipoPassageiro tipo) {
+        getHibernateTemplate().saveOrUpdate(tipo);
+    }
 
-	public String getHqlBuscarTipoPassagPorCartao() {
-		return hqlBuscarTipoPassagPorCartao;
-	}
+    public void criarTipoPassageiro(TipoPassageiro tipo) {
+        salvarTipoPassageiro(tipo);
+    }
 
-	public void setHqlBuscarTipoPassagPorCartao(
-			String hqlBuscarTipoPassagPorCartao) {
-		this.hqlBuscarTipoPassagPorCartao = hqlBuscarTipoPassagPorCartao;
-	}
+    public void alterarTipoPassageiro(TipoPassageiro tipo) {
+        salvarTipoPassageiro(tipo);
+    }
 
-	public String getHqlBuscarTipoPorNome() {
-		return hqlBuscarTipoPorNome;
-	}
+    public TipoPassageiro buscarTipoPassageiro(int tipoID) {
+        return (TipoPassageiro) getHibernateTemplate().get(TipoPassageiro.class, tipoID);
+    }
 
-	public void setHqlBuscarTipoPorNome(String hqlBuscarTipoPorNome) {
-		this.hqlBuscarTipoPorNome = hqlBuscarTipoPorNome;
-	}
+    public List buscarTiposPassageiros() {
+        return getHibernateTemplate().loadAll(TipoPassageiro.class);
+    }
 
-	public void salvarTipoPassageiro(TipoPassageiro tipo) {
-		getHibernateTemplate().saveOrUpdate(tipo);
-	}
+    public TipoPassageiro buscarTipoPassageiro(String nomeTipo) {
+        DetachedCriteria tipoPorNome = DetachedCriteria.forClass(TipoPassageiro.class);
+        tipoPorNome.add(Restrictions.eq("nomeTipo", nomeTipo));
+        List tipos = getHibernateTemplate().findByCriteria(tipoPorNome);
+        return (TipoPassageiro) tipos.get(0);
+    }
 
-	public void criarTipoPassageiro(TipoPassageiro tipo) {
-		salvarTipoPassageiro(tipo);
-	}
+    public void removerTipoPassageiro(TipoPassageiro tipo) {
+        getHibernateTemplate().delete(tipo);
+    }
 
-	public void alterarTipoPassageiro(TipoPassageiro tipo) {
-		salvarTipoPassageiro(tipo);
-	}
+    public TipoPassageiro buscarTipoPassagPorCartao(int cartaoID) {
+        DetachedCriteria tipoPorCartao = DetachedCriteria.forClass(TipoPassageiro.class);
+        tipoPorCartao.add(Restrictions.eq("cartaoID", cartaoID));
+        List tipos = getHibernateTemplate().findByCriteria(tipoPorCartao);
+        return (TipoPassageiro) tipos.get(0);
+    }
 
-	public TipoPassageiro buscarTipoPassageiro(int tipoID) {
-		return (TipoPassageiro) getHibernateTemplate().get(
-				TipoPassageiro.class, new Integer(tipoID));
-	}
+    public List buscarTodosTipos() {
+        return getHibernateTemplate().loadAll(TipoPassageiro.class);
+    }
 
-	public List buscarTiposPassageiros() {
-		return getHibernateTemplate().loadAll(TipoPassageiro.class);
-	}
+    public List buscarTiposPermitidos(List tiposJaAdquiridos) {
+        List tiposPermitidos = buscarTodosTipos();
+        tiposPermitidos.removeAll(tiposJaAdquiridos);
+        return tiposPermitidos;
+    }
 
-	public TipoPassageiro buscarTipoPassageiro(String nomeTipo) {
-		List tipos = getHibernateTemplate().findByNamedParam(
-				hqlBuscarTipoPorNome, "nomeTipo", nomeTipo);
-		return (TipoPassageiro) tipos.get(0);
-	}
-
-	public void removerTipoPassageiro(TipoPassageiro tipo) {
-		getHibernateTemplate().delete(tipo);
-	}
-
-	public TipoPassageiro buscarTipoPassagPorCartao(int cartaoID) {
-		List tipos = getHibernateTemplate().find(hqlBuscarTipoPassagPorCartao,
-				new Integer(cartaoID));
-		return (TipoPassageiro) tipos.get(0);
-	}
-
-	public List buscarTodosTipos() {
-		return getHibernateTemplate().loadAll(TipoPassageiro.class);
-	}
-
-	public List buscarTiposPermitidos(List tiposJaAdquiridos) {
-		// List todosTipos = buscarTodosTipos();
-		// List tiposPermitidos = new ArrayList();
-
-		List tiposPermitidos = buscarTodosTipos();
-		tiposPermitidos.removeAll(tiposJaAdquiridos);
-
-		if (getCombinarCartoes())
-		for (Iterator it = tiposJaAdquiridos.iterator(); it.hasNext();) {
-			TipoPassageiro tipo = (TipoPassageiro) it.next();
-			tiposPermitidos.removeAll(tipo.getTiposIncompativeis());
-		}
-
-		// for (Iterator it = todosTipos.iterator(); it.hasNext();) {
-		// TipoPassageiro tipo = (TipoPassageiro) it.next();
-		// if (!tiposJaAdquiridos.contains(tipo)){
-		// tiposPermitidos.add(tipo);
-		// }
-		// }
-
-		return tiposPermitidos;
-	}
-
-	public void removerTipoPassageiro(int tipoID) {
-		TipoPassageiro tipo = buscarTipoPassageiro(tipoID);
-		removerTipoPassageiro(tipo);
-	}
-
-	public void setCombinarCartoes(Boolean combinarCartoes) {
-		this.combinarCartoes = combinarCartoes;
-	}
-
-	public Boolean getCombinarCartoes() {
-		return combinarCartoes;
-	}
+    public void removerTipoPassageiro(int tipoID) {
+        TipoPassageiro tipo = buscarTipoPassageiro(tipoID);
+        removerTipoPassageiro(tipo);
+    }
 
 }
